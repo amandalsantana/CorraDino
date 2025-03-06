3a896f48adca2805a5d0dfba898956a240f5bfb2
Amanda Santana <alsantana11@gmail.com>
2025-03-06 16:40:37 -0300

    Creating asset folder Inserting all assets Creating menu Creating const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_ORANGE = (225, 113, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
+# M
+MENU_OPTION = ('NOVO JOGO 1P',
+               'NOVO JOGO 2P - ALIADO',
+               'NOVO JOGO 2P - INIMIGO',
+               'PONTUACAO',
+               'SAIR')
+
+# W
+WIN_WIDTH = 493
+WIN_HEIGHT = 417
